1abdb3be4ace1f1e2a731c9c32ef999c47dd103d
Carlo Piana <carlo@piana.eu>
2020-02-29 17:06:04 +0100

    structure: use two ways to have background images declared

--- reveal-md/assets/js/frag-image.js
+++ reveal-md/assets/js/frag-image.js
@@ -4,7 +4,7 @@ module.exports = (markdown, options) => {
       markdown
         .split('\n')
         .map((line, index) => {
-          return line.replace(/<!--\ ?bkg\ ?-->\ ?(.*)$/g, '<!-- .slide:  data-background-image="assets/img/$1" -->')
+          return line.replace(/<!--\ ?bkg\ (.*)\.(jpg|png|svg|webp)-->$/g, '<!-- .slide:  data-background-image="assets/img/$1.$2" -->')
           .replace(/^\+(.*\.)(jpg|png|svg|webp)/g, '<!-- .slide:  data-background-image="assets/img/$1$2" -->')
           .replace(/<!--\ ?frag\ ?-->/g, '&shy;<!-- .element: class="fragment" -->');
         })
