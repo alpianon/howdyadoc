22dc4e504406ff6d90e2bf75661e680c45e16cbf
kappapiana <carlo@piana.eu>
2020-04-21 23:32:41 +0200

    structure: allow adding class to fragments

--- reveal-md/assets/js/frag-image.js
+++ reveal-md/assets/js/frag-image.js
@@ -6,7 +6,7 @@ module.exports = (markdown, options) => {
         .map((line, index) => {
           return line.replace(/<!--\ ?bkg\ ?(.*)\.(jpg|png|svg|webp)\ ?-->/g, '<!-- .slide:  data-background-image="assets/img/$1.$2" -->')
           .replace(/^\+(.*\.)(jpg|png|svg|webp)/g, '<!-- .slide:  data-background-image="assets/img/$1$2" -->')
-          .replace(/<!--\ ?frag\ ?-->/g, '&shy;<!-- .element: class="fragment" -->')
+          .replace(/<!--\ ?frag(\ ?.*?)-->/g, '&shy;<!-- .element: class="fragment$1" -->')
           .replace(/@fa\[(.*?)\]/g, '<i class="fa fa-$1"></i>');
         })
         .join('\n')
