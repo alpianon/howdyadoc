3228b890a69e885e2347d9d23a714411f61d0209
kappapiana <carlo@piana.eu>
2020-06-30 10:39:51 +0200

    structure: class center now at fragment level with comment

--- reveal-md/assets/js/frag-image.js
+++ reveal-md/assets/js/frag-image.js
@@ -8,6 +8,7 @@ module.exports = (markdown, options) => {
           .replace(/^\+(.*\.)(jpg|png|svg|webp)/g, '<!-- .slide:  data-background-image="assets/img/$1$2" -->')
           .replace(/<!--\ ?frag(\ ?.*?)\ ?index ([0-9])\ ?-->/g, '&shy;<!-- .element: class="fragment$1" data-fragment-index="$2"-->')
           .replace(/<!--\ ?frag(\ ?.*?)-->/g, '&shy;<!-- .element: class="fragment$1" -->')
+          .replace(/<!--\ ?center\ ?-->/g, '&shy;<!-- .element: class="center" -->')
           // .replace(/<!--\ ?id (.*?)\ ?-->/g, '<!-- .slide: id="$1" -->')
           .replace(/<!--\ ?id (.*)\ ?-->/g, '<!-- .slide: id="$1" -->')
           .replace(/@fa\[(.*?)\]/g, '<i class="fa fa-$1"></i>')
